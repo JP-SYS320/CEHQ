3572197a4854543a50b3628f2be53de2edf64d9f
JP-SYS320 <jptremblay18@hotmail.com>
2026-05-22 20:21:31 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@
  * un re-téléchargement chez tous les utilisateurs.
  */
 
-const CACHE_VERSION = 'v10';
+const CACHE_VERSION = 'v11';
 const APP_CACHE = `currentrun-app-${CACHE_VERSION}`;
 const DATA_CACHE = `currentrun-data-${CACHE_VERSION}`;
 
